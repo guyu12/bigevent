1ea3118af39fa5a0f62a51829cdaf0e334d8a03c
guyu <2608716115@qq.com>
2020-09-09 20:36:25 +0800

    登陆和注册ajax请求完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -13,6 +13,7 @@ $(function() {
     // 自定义校验规则
     // 获取layui的form表单模块
     var form = layui.form;
+    var layer = layui.layer;
     // 调用verify方法，定义验证规则
     form.verify({
         // 校验密码
@@ -27,8 +28,50 @@ $(function() {
             }
         }
     });
+    // 监听表单注册提交事件
+    $('#form_reg').on('submit', function(e) {
+        // 阻止默认行为
+        e.preventDefault();
+        // console.log($('.reg_box [name="username"]').val());
+        $.ajax({
+            url: 'http://ajax.frontend.itheima.net/api/reguser',
+            type: 'post',
+            data: {
+                username: $('.reg_box [name="username"]').val(),
+                password: $('.reg_box [name="password"]').val()
+            },
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message);
+                }
+                layer.msg('注册成功，请登录');
+                $('#link_login').click();
+            }
+        })
+    })
+
+    // 监听表单登陆提交事件
+    $('#form_login').on('submit', function(e) {
+        // 阻止默认行为
+        console.log(111);
+        e.preventDefault();
+
+        $.ajax({
+            url: 'http://ajax.frontend.itheima.net/api/login',
+            type: 'post',
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message);
+                }
+                layer.msg('登陆成功');
+                console.log(res.token);
+                location.href = './index.html';
+            }
+        })
 
 
+    })
 
 
 
